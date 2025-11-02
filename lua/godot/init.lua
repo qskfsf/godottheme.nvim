ac6a2973d527ebeaad7336c0e81a1436e8a0de49
Rehd0 <rehd070@gmail.com>
2025-11-02 05:00:57 +0100

    enhanced highlighting with treesiter and lsp

--- lua/godot/init.lua
+++ lua/godot/init.lua
@@ -62,21 +62,53 @@ function M.setup()
     Search = { bg = colors.transparentBlue },
     StatusLine = { fg = colors.lightTheme },
 
-    Tag = { fg = colors.blue },
+    -- ==================================================
+    -- Popup menu (autocomplete, command line, etc.) for cmp
+    -- ==================================================
+    Pmenu        = { fg = colors.fg, bg = colors.lightGray },
+    PmenuSel     = { fg = colors.bg, bg = colors.funcCallBlue },
+    PmenuSbar    = { bg = colors.darkGray },
+    PmenuThumb   = { bg = colors.funcCallBlue },
+    PmenuKind    = { fg = colors.cyan },
+    PmenuExtra   = { fg = colors.gray },
+    -- for Coc
+    CocMenuSel   = { fg = colors.bg, bg = colors.funcCallBlue, bold = true },
+    CocFloating  = { bg = colors.lightGray },
+    CocPumMenu   = { fg = colors.fg, bg = colors.lightGray },
+    CocSearch    = { fg = colors.yellow, bg = colors.transparentBlue, bold = true },
 
+    -- ==================================================
     -- Treesitter support
-    ["@string.special.url"] = { fg = colors.darkGreen },
-    ["@function.builtin"] = { fg = colors.flowerBlue },
+    -- ==================================================
+    ["@string.special.url"]  = { fg = colors.darkGreen },
+    ["@function.builtin"]    = { fg = colors.flowerBlue },
     ["@keyword.conditional"] = { fg = colors.pink },
-    ["@keyword.repeat"] = { fg = colors.pink },
-    ["@keyword.exception"] = { fg = colors.pink },
-    ["@type.builtin"] = { fg = colors.typeGreen },
-    ["@keyword.return"] = { fg = colors.pink },
-    ["@function.call"] = { fg = colors.funcCallBlue },
+    ["@keyword.repeat"]      = { fg = colors.pink },
+    ["@keyword.exception"]   = { fg = colors.pink },
+    ["@type.builtin"]        = { fg = colors.typeGreen },
+    ["@keyword.return"]      = { fg = colors.pink },
+    ["@function.call"]       = { fg = colors.funcCallBlue },
+
+    -- HTML / CSS PHP XNL
+    Tag = { fg = colors.red },
+    ["@htmlLink"] = { fg = colors.errorYellow },
+    ["@tag.delimiter"]          = { fg = colors.blue },
+    ["@tag.attribute"]          = { fg = colors.blue },
+    ["@tag.string"]             = { fg = colors.yellow },
+    ["@tag.property"] = { fg = colors.flowerBlue },
+
+    -- Treesitter context
+    TreesitterContext         = { fg = colors.fg, bg = colors.lightGray },
+    TreesitterContextLineNumber = { fg = colors.gray, bg = colors.lightGray },
+
+    -- indent Blank line
+    IblScope = { fg = colors.blue },
+
   }
 
-
+  -- ==================================================
   -- Apply highlights
+  -- ==================================================
   for group, opts in pairs(highlights) do
     vim.api.nvim_set_hl(0, group, opts)
   end
